5dd48cae33aefdd0bf6ccddbce5dfb9a755f122f
Lioncat2002 <sankhayan2002@gmail.com>
2022-11-03 00:19:42 +0530

    feat: rewrite the benchmarking to remove bencher

--- light-rpc/benches/metrics.rs
+++ light-rpc/benches/metrics.rs
@@ -1,40 +1,28 @@
+use std::time::SystemTime;
+#[cfg(test)]
 use {
-    bencher::{benchmark_group, benchmark_main, Bencher, TestDescAndFn},
-    csv::Writer,
     light_rpc::LightRpc,
-    serde::Serialize,
+    solana_sdk::commitment_config::CommitmentConfig,
+    solana_sdk::signature::Signature,
     solana_sdk::{
-        commitment_config::CommitmentConfig,
-        native_token::LAMPORTS_PER_SOL,
-        pubkey::Pubkey,
-        signature::{Signature, Signer},
-        signer::keypair::Keypair,
-        system_instruction,
-        transaction::Transaction,
+        native_token::LAMPORTS_PER_SOL, signature::Signer, signer::keypair::Keypair,
+        system_instruction, transaction::Transaction,
     },
-    std::{fs::OpenOptions, io::Write},
+    std::{thread, time::Duration},
 };
 
 const RPC_ADDR: &str = "127.0.0.1:8899";
 const TPU_ADDR: &str = "127.0.0.1:1027";
 const CONNECTION_POOL_SIZE: usize = 1;
 
-fn confirm_transaction_sender(
-    light_rpc: &LightRpc,
-    signature: &Signature,
-    mut retries: u16,
-) -> bool {
-    while retries > 0 {
-        let confirmed = light_rpc
-            .confirm_transaction(signature, CommitmentConfig::confirmed())
-            .unwrap()
-            .value;
-        if confirmed {
-            return true;
-        }
-        retries -= 1;
-    }
-    false
+#[derive(serde::Serialize)]
+struct Metrics {
+    #[serde(rename = "start time(ns)")]
+    start_time: u128,
+    #[serde(rename = "end time(ns)")]
+    end_time: u128,
+    #[serde(rename = "duration(ns)")]
+    duration: u128,
 }
 
 fn forward_transaction_sender(
@@ -51,7 +39,7 @@ fn forward_transaction_sender(
         .request_airdrop(&frompubkey, LAMPORTS_PER_SOL)
     {
         Ok(sig) => {
-            let confirmed = confirm_transaction_sender(&light_rpc, &sig, 240);
+            let confirmed = confirm_transaction_sender(&light_rpc, &sig, 300);
         }
         Err(_) => println!("Error requesting airdrop"),
     };
@@ -68,42 +56,60 @@ fn forward_transaction_sender(
     let signature = light_rpc.forward_transaction(txn).unwrap();
     signature
 }
-fn bench_forward_transaction_confirm_transaction(b: &mut Bencher) {
+
+fn confirm_transaction_sender(
+    light_rpc: &LightRpc,
+    signature: &Signature,
+    mut retries: u16,
+) -> bool {
+    while retries > 0 {
+        let confirmed = light_rpc
+            .confirm_transaction(signature, CommitmentConfig::confirmed())
+            .unwrap()
+            .value;
+        if confirmed {
+            return true;
+        }
+        retries -= 1;
+        thread::sleep(Duration::from_millis(500))
+    }
+    false
+}
+
+fn test_forward_transaction_confirm_transaction(tps: u64) {
     let light_rpc = LightRpc::new(
         RPC_ADDR.parse().unwrap(),
         TPU_ADDR.parse().unwrap(),
         CONNECTION_POOL_SIZE,
     );
     let alice = Keypair::new();
-    let frompubkey = Signer::pubkey(&alice);
-
     let bob = Keypair::new();
-    let topubkey = Signer::pubkey(&bob);
 
-    let lamports = 1_000_00;
-    let mut sig = Signature::default();
-    let mut file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .append(true)
-        .open("metrics.csv")
-        .unwrap();
-    let mut wtr = csv::Writer::from_writer(file);
-    b.iter(|| {
-        sig = forward_transaction_sender(&light_rpc, &alice, &bob, lamports);
-    });
-    let forwarder_time = (b.ns_per_iter() as f64) / 1000000 as f64;
-    b.iter(|| {
-        confirm_transaction_sender(&light_rpc, &sig, 240);
-    });
-    let confirm_time = (b.ns_per_iter() as f64) / 1000000 as f64;
+    let lamports = 1_000_000;
+    let mut data: Vec<Metrics> = vec![];
 
-    wtr.write_record(&[
-        forwarder_time.to_string(),
-        confirm_time.to_string(),
-        (forwarder_time - confirm_time).abs().to_string(),
-    ])
-    .unwrap();
+    let mut wtr = csv::Writer::from_path("metrics.csv").unwrap();
+    let instant = SystemTime::now();
+    for _ in 0..tps {
+        let start_time = instant.elapsed().unwrap().as_nanos();
+
+        let signature = forward_transaction_sender(&light_rpc, &alice, &bob, lamports);
+
+        let end_time = instant.elapsed().unwrap().as_nanos();
+
+        data.push(Metrics {
+            start_time,
+            end_time,
+            duration: end_time - start_time,
+        });
+    }
+    for d in data.into_iter() {
+        wtr.serialize(d).unwrap();
+    }
 }
-benchmark_group!(benches, bench_forward_transaction_confirm_transaction);
-benchmark_main!(benches);
+#[test]
+fn dummy() {}
+
+fn main() {
+    test_forward_transaction_confirm_transaction(100);
+}
