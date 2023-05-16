830f3f87366cefeeabb065e88d90c42d1279a38d
steve-gg <grooviegermanikus@gmail.com>
2023-05-16 21:33:24 +0200

    reset sysvar.rs test

--- program-test/tests/sysvar.rs
+++ program-test/tests/sysvar.rs
@@ -38,8 +38,7 @@ async fn get_sysvar() {
 
     let mut context = program_test.start_with_context().await;
     context.warp_to_slot(42).unwrap();
-    // let instructions = vec![Instruction::new_with_bincode(program_id, &(), vec![])];
-    let instructions = vec![];
+    let instructions = vec![Instruction::new_with_bincode(program_id, &(), vec![])];
 
     let transaction = Transaction::new_signed_with_payer(
         &instructions,
